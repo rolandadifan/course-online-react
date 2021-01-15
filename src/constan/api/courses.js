1f4f5c5a1bdd60d224b08d9444a48056266aec34
Rolanda <46946118+rolandadifan@users.noreply.github.com>
2021-01-15 16:09:32 +0700

    update3

--- src/constan/api/courses.js
+++ src/constan/api/courses.js
@@ -1,8 +1,7 @@
-import axios from 'configs/axios'
-
+import axios from 'configs/axios';
 export default {
     details: (id) => axios.get(`/courses/${id}`).then((res) => res.data),
 
     join: (id) => axios.post("/my-course", { course_id: id }),
-    mine: () => axios.get("/my-course"),
+    mine: () => axios.get("/my-course")
 };
